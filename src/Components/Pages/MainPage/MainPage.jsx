1ff6045d2622a80531f9682bbb07ecf0b82a4b23
Назар Арчаков <nazararcakov@Nazars-MacBook-Pro.local>
2021-04-15 01:55:09 +0300

    feat: add main screen animations and preview modal

--- src/Components/Pages/MainPage/MainPage.jsx
+++ src/Components/Pages/MainPage/MainPage.jsx
@@ -1,10 +1,21 @@
-import { motion, transform } from 'framer-motion';
-import React from 'react';
+import { motion } from 'framer-motion';
+import React, { useEffect, useRef } from 'react';
 import { mainPageAnimations } from '../../../animations';
 import Header from '../../Common/Header/Header';
 import './MainPage.scss';
+import lottie from 'lottie-web';
 
 const MainPage = () => {
+  const lottieContainer = useRef();
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: lottieContainer.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: require('../../../animations/resume-animation.json'),
+    });
+  }, []);
   return (
     <motion.div
       initial='out'
@@ -13,31 +24,21 @@ const MainPage = () => {
       variants={mainPageAnimations}>
       <Header />
       <div className='container'>
-        <div className='intro'>
-          <motion.h1
-            initial={{ translateX: '-200%' }}
-            animate={{ translateX: '0%' }}
-            transition={{ type: 'tween', duration: 1 }}
-            className='intro__title'>
-            Choose and build your own CV in just 20 minutes!
-          </motion.h1>
-          <motion.p
-            initial={{ translateX: '-200%' }}
-            animate={{ translateX: '0%' }}
-            transition={{ type: 'tween', duration: 1, delay: 0.2 }}
-            className='intro__description'>
-            I'm baby schlitz hammock wayfarers sustainable coloring book ennui.
-            Tacos roof party organic art party. Art party woke flannel pabst
-            keffiyeh.
-          </motion.p>
-          <motion.a
-            initial={{ translateX: '-200%' }}
-            animate={{ translateX: '0%' }}
-            transition={{ type: 'tween', duration: 1, delay: 0.4 }}
-            className='intro__button'
-            href='#nowhear'>
-            Get Template
-          </motion.a>
+        <div className='hero'>
+          <div className='hero__intro'>
+            <motion.h1 className='intro__title'>
+              Choose and build your own CV in just 20 minutes!
+            </motion.h1>
+            <motion.p className='intro__description'>
+              I'm baby schlitz hammock wayfarers sustainable coloring book
+              ennui. Tacos roof party organic art party. Art party woke flannel
+              pabst keffiyeh.
+            </motion.p>
+            <motion.a className='intro__button' href='#nowhear'>
+              Get Template
+            </motion.a>
+          </div>
+          <div ref={lottieContainer} className='hero__lottie'></div>
         </div>
       </div>
     </motion.div>
