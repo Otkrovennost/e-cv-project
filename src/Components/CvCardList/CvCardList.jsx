3834563f7416d94c11fc90b424597d54aa2ca391
Назар Арчаков <nazararcakov@Nazars-MacBook-Pro.local>
2021-04-17 12:15:59 +0300

    fix: bug with modal window

--- src/Components/CvCardList/CvCardList.jsx
+++ src/Components/CvCardList/CvCardList.jsx
@@ -1,16 +1,16 @@
-import React, { useContext, useState } from 'react';
-import CvCard from '../CvCard/CvCard';
-import './CvCardList.scss';
-import { cvCards } from '../../data';
-import { CardsContext } from '../../context/CvContext';
-import Modal from '../Common/Modal/Modal';
+import React, { useContext, useState } from "react";
+import CvCard from "../CvCard/CvCard";
+import "./CvCardList.scss";
+import { cvCards } from "../../data";
+import { CardsContext } from "../../context/CvContext";
+import Modal from "../Common/Modal/Modal";
 
 function CvCardList({ props }) {
   const { cvClickHandler, selectedCv } = useContext(CardsContext);
   const [isOpened, setIsOpened] = useState(false);
   console.log(selectedCv);
   return (
-    <div className='cv-card_list'>
+    <div className="cv-card_list">
       {cvCards.map((item) => {
         return (
           <CvCard
@@ -26,13 +26,13 @@ function CvCardList({ props }) {
         );
       })}
       <Modal setIsOpened={setIsOpened} open={isOpened}>
-        <div className='modal__content'>
-          <div className='modal__image-wrapper'>
-            <img src={selectedCv.cvImage} alt='' className='modal__img' />
+        <div className="modal__content">
+          <div className="modal__image-wrapper">
+            <img src={selectedCv?.cvImage} alt="" className="modal__img" />
           </div>
-          <div className='modal__text'>
-            <h3 className='modal__title'>{selectedCv.cvTitle}</h3>
-            <p className='modal__description'>
+          <div className="modal__text">
+            <h3 className="modal__title">{selectedCv?.cvTitle}</h3>
+            <p className="modal__description">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Temporibus corrupti, nobis adipisci sed vero ipsam sapiente? Autem
               libero odit fuga eveniet animi omnis qui adipisci enim, facere,
@@ -40,9 +40,10 @@ function CvCardList({ props }) {
             </p>
             <button
               onClick={() => {
-                props.history.push(`/builder/${selectedCv.id}`);
+                props.history.push(`/builder/${selectedCv?.id}`);
               }}
-              className='modal__btn'>
+              className="modal__btn"
+            >
               Start Editing
             </button>
           </div>
