4c16d355411471b610476cd090e3d40c782b1100
Назар Арчаков <nazararcakov@Nazars-MacBook-Pro.local>
2021-04-17 16:07:28 +0300

    feat: add hel-and-tips page

--- src/Components/Common/NavList/NavList.jsx
+++ src/Components/Common/NavList/NavList.jsx
@@ -1,28 +1,31 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-import { AppRoute } from '../../../constants';
+import { AppRoute } from "../../../constants";
 
-import './NavList.scss';
+import "./NavList.scss";
 
 const NavList = () => {
   return (
-    <ul className='nav__list'>
-      <li className='nav__item'>
-        <NavLink className='nav__link' to={AppRoute.CV_TEMPLATES_PAGE}>
+    <ul className="nav__list">
+      <li className="nav__item">
+        <NavLink className="nav__link" to={AppRoute.CV_TEMPLATES_PAGE}>
           CV Templates
         </NavLink>
       </li>
-      <li className='nav__item'>
-        <NavLink
-          className='nav__link'
-          to={AppRoute.BUSINESS_CARD_PAGE}>
+      <li className="nav__item">
+        <NavLink className="nav__link" to={AppRoute.BUSINESS_CARD_PAGE}>
           Business Cards
         </NavLink>
       </li>
-      <li className='nav__item'>
-        <NavLink className='nav__link' to={AppRoute.ABOUT_PAGE}>
-          About
+      <li className="nav__item">
+        <NavLink className="nav__link" to={AppRoute.HELP_PAGE}>
+          CV Help &amp; Tips
+        </NavLink>
+      </li>
+      <li className="nav__item">
+        <NavLink className="nav__link" to={AppRoute.HELP_PAGE}>
+          Login | Register
         </NavLink>
       </li>
     </ul>
