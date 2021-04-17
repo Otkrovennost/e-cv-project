d3fc6559afe63eb487f2a252bbd0d4a31904935c
Назар Арчаков <nazararcakov@Nazars-MacBook-Pro.local>
2021-04-17 13:24:11 +0300

    feat: fix footer

--- src/Components/CvCard/CvCard.jsx
+++ src/Components/CvCard/CvCard.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import './CvCard.scss';
+import React from "react";
+import "./CvCard.scss";
 
 function CvCard({ name, image, getId, setIsOpened, isOpened }) {
   return (
-    <div onClick={getId} className='cv-card'>
-      <div className='cv-card__text-content'>
-        <h4 className='cv-card__title'>{name}</h4>
+    <div onClick={getId} className="cv-card">
+      <div className="cv-card__text-content">
+        <h4 className="cv-card__title">{name}</h4>
       </div>
-      <div className='cv-card__body'>
-        <div className='cv-card__image'>
-          <img src={image} alt='CV Template' />
+      <div className="cv-card__body">
+        <div className="cv-card__image">
+          <img src={image} alt="CV Template" />
         </div>
-        <div className='cv-card__colors'>
+        {/* <div className='cv-card__colors'>
           <div className='cv-card__color-outer'>
             <div className='cv-card__color-inner'></div>
           </div>
@@ -23,8 +23,8 @@ function CvCard({ name, image, getId, setIsOpened, isOpened }) {
           <div className='cv-card__color-outer'>
             <div className='cv-card__color-inner'></div>
           </div>
-        </div>
-        <button onClick={() => setIsOpened(true)} className='cv-card__button'>
+        </div> */}
+        <button onClick={() => setIsOpened(true)} className="cv-card__button">
           Preview
         </button>
       </div>
