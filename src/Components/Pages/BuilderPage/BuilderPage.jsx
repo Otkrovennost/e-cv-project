5b4e2035bb2ca07f04b436d56fdba1d36e853d04
Назар Арчаков <nazararcakov@Nazars-MacBook-Pro.local>
2021-04-27 03:00:49 +0300

    feat: add authorization and fixed bugs

--- src/Components/Pages/BuilderPage/BuilderPage.jsx
+++ src/Components/Pages/BuilderPage/BuilderPage.jsx
@@ -1,43 +1,40 @@
-import React, { useContext, useState } from "react";
-import { PDFViewer } from "@react-pdf/renderer";
-import {
-  Button,
-  IconButton
-} from "@material-ui/core";
+import React, { useContext, useState } from 'react';
+import { PDFViewer } from '@react-pdf/renderer';
+import { Button, IconButton } from '@material-ui/core';
 import AssignmentIcon from '@material-ui/icons/Assignment';
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
-import Header from "../../Common/Header/Header";
-import Footer from "../../Common/Footer/Footer";
-import BuilderForm from "../../BuilderForm/BuilderForm";
+import Header from '../../Common/Header/Header';
+import Footer from '../../Common/Footer/Footer';
+import BuilderForm from '../../BuilderForm/BuilderForm';
 
-import { CardsContext } from "../../../context/CvContext.js";
+import { CardsContext } from '../../../context/CvContext.js';
 
-import "./BuilderPage.scss";
+import './BuilderPage.scss';
 
 const useStyles = makeStyles((theme) => ({
   buttonShow: {
     display: 'none',
-    [theme.breakpoints.down("sm")]: {
+    [theme.breakpoints.down('sm')]: {
       position: 'fixed',
       display: 'flex',
-      justifyContent: 'center', 
-      bottom: '70px', 
-      right: '20px', 
-      padding: '20px', 
+      justifyContent: 'center',
+      bottom: '70px',
+      right: '20px',
+      padding: '20px',
       color: '#fff',
       backgroundColor: 'rgb(114, 172, 145)',
       cursor: 'pointer',
       '&:hover': {
         backgroundColor: 'rgb(114, 172, 145)',
-      }
+      },
     },
   },
-}))
+}));
 
 export default function BuilderPage() {
   const classes = useStyles();
-  const [isShownResume, setIsShownResume] = useState(false)
+  const [isShownResume, setIsShownResume] = useState(false);
 
   const { renderCard, selectedCv, cvData, setCvData } = useContext(
     CardsContext
@@ -45,24 +42,34 @@ export default function BuilderPage() {
   return (
     <div className="builder-page">
       <Header />
-      <div className="container" style={{position: 'relative'}}>
+      <div className="container" style={{ position: 'relative' }}>
         <div className="builder-page__inner">
-          <BuilderForm cvData={cvData} setCvData={setCvData} isShownResume={isShownResume}/>
-          <div className={isShownResume ? "builder-page__pdf-viewer-view" : "builder-page__pdf-viewer"}>
+          <BuilderForm
+            cvData={cvData}
+            setCvData={setCvData}
+            isShownResume={isShownResume}
+          />
+          <div
+            className={
+              isShownResume
+                ? 'builder-page__pdf-viewer-view'
+                : 'builder-page__pdf-viewer'
+            }
+          >
             <PDFViewer>{renderCard(selectedCv.cvName)}</PDFViewer>
           </div>
         </div>
-        <Button 
+        <Button
           className={classes.buttonShow}
           onClick={() => {
-            setIsShownResume(prev => !prev)
+            setIsShownResume((prev) => !prev);
           }}
         >
-          <AssignmentIcon style={{color: '#fff', marginRight: '20px'}}/>
+          <AssignmentIcon style={{ color: '#fff', marginRight: '20px' }} />
           {isShownResume ? 'Return to cv-form' : 'Preview & Download'}
         </Button>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
--- src/Components/Pages/BusinessCardsPage/BusinessCardList/BusinessCardList.jsx
+++ src/Components/Pages/BusinessCardsPage/BusinessCardList/BusinessCardList.jsx
@@ -1,14 +1,12 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import { BusinessCardsContext } from "../../../../context/BusinessCardContext";
+import React, { useContext, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { BusinessCardsContext } from '../../../../context/BusinessCardContext';
 import Loader from '../../../Common/loader/Loader';
 
 import './BusinessCardList.scss';
 
-const BusinessCardList = ({props}) => {
-  const { cardClickHandler, selectedCard } = useContext(
-    BusinessCardsContext
-  );
+const BusinessCardList = ({ props }) => {
+  const { cardClickHandler, selectedCard } = useContext(BusinessCardsContext);
 
   const [cvBusinessCards, setCvBusinessCards] = useState(null);
 
@@ -18,9 +16,9 @@ const BusinessCardList = ({props}) => {
       .then((data) => {
         setCvBusinessCards(data);
       });
-  }, []); 
+  }, []);
 
-  console.log(cvBusinessCards)
+  console.log(cvBusinessCards);
 
   return (
     <React.Fragment>
@@ -29,22 +27,26 @@ const BusinessCardList = ({props}) => {
           {cvBusinessCards.map((elem) => (
             <li className="card__item" key={elem.id}>
               <div className="card__body">
-              <Link 
-                onClick={(e) => {
-                  cardClickHandler(e, elem)
-                  props.history.push(`/creator/${elem.id}`);
-                }}
-              >
-                <img className="card__img" src={`data:image/jpeg;base64, ${elem.cardImage}`} alt="Preview business card" />
-              </Link>
+                <Link
+                  onClick={(e) => {
+                    cardClickHandler(e, elem);
+                    props.history.push(`/creator/${elem.id}`);
+                  }}
+                >
+                  <img
+                    className="card__img"
+                    src={`data:image/jpeg;base64, ${elem.cardImage}`}
+                    alt="Preview business card"
+                  />
+                </Link>
               </div>
             </li>
           ))}
-        </ul>)
-        : (
-        <Loader/>
+        </ul>
+      ) : (
+        <Loader />
       )}
     </React.Fragment>
-  )
-}
+  );
+};
 export default BusinessCardList;
